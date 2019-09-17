8262eb7befe047cffa0a23c6c00e3604f44599de
sprajjwal <sprajjwal@ucdavis.edu>
2019-09-16 20:46:14 -0700

    [ADD] Jump added

--- firstGame.py
+++ firstGame.py
@@ -5,13 +5,16 @@ win=pygame.display.set_mode((500,500))
 pygame.display.set_caption("First game")
 
 x = 50
-y = 50
+y = 440
 width = 40
 height = 60
 vel = 5
 run = True
+isJump = False
+jumpCount = 10
+
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,15 +22,29 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > 0:
         x -= vel
 
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500-width:
         x += vel
-    if keys[pygame.K_UP]:
-        y-=vel
-    if keys[pygame.K_DOWN]:
-        y+=vel
+    if not isJump:
+        if keys[pygame.K_UP] and y > 0:
+            y-=vel
+        if keys[pygame.K_DOWN] and y < 500-height:
+            y+=vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount  >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount **2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+    
     win.fill((0,0,0))
     pygame.draw.rect(win, (255, 222, 0), (x, y, width, height))
     pygame.display.update()
